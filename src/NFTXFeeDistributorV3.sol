cf20602ae94dfbdc62c3b31e98ecd80bd04183ef
apoorvlathey <latheyapoorv@gmail.com>
2023-08-01 01:52:57 +0200

    FeeDistributor: fix leftover calculation

--- src/NFTXFeeDistributorV3.sol
+++ src/NFTXFeeDistributorV3.sol
@@ -116,7 +116,7 @@ contract NFTXFeeDistributorV3 is
                 vaultId,
                 vault
             );
-            leftover += tokenSent ? 0 : wethAmountToSend;
+            leftover = tokenSent ? 0 : leftover + wethAmountToSend;
 
             unchecked {
                 ++i;
