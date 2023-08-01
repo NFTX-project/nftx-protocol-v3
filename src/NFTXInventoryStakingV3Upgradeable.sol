f14021ba958a25bceab58565922602f24aa42071
apoorvlathey <latheyapoorv@gmail.com>
2023-08-01 14:31:05 +0200

    InventoryStaking: only allow position owner to increasePosition

--- src/NFTXInventoryStakingV3Upgradeable.sol
+++ src/NFTXInventoryStakingV3Upgradeable.sol
@@ -248,6 +248,8 @@ contract NFTXInventoryStakingV3Upgradeable is
         bool viaPermit2,
         bool forceTimelock
     ) external {
+        if (ownerOf(positionId) != msg.sender) revert NotPositionOwner();
+
         Position storage position = positions[positionId];
 
         uint256 vaultId = position.vaultId;
