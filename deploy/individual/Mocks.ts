514ccfc9aedaf9878c857abb987e9be73eabee5e
apoorvlathey <latheyapoorv@gmail.com>
2023-09-11 21:36:19 +0530

    update MockNFTs with base tokenURI

--- deploy/individual/Mocks.ts
+++ deploy/individual/Mocks.ts
@@ -8,17 +8,29 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await getNamedAccounts();
 
   const nft = await deploy("MockRoyaltyNFT", {
-    args: ["CryptoPunks", "CryptoPunks"],
+    args: [
+      "CryptoPunks",
+      "CryptoPunks",
+      "https://ipfs.io/ipfs/QmcsX1EprrBwjPRT7dyDWvmGX7zvcRsmKKDaMTiFbSsZh2/",
+    ],
     from: deployer,
     log: true,
   });
   await deploy("MockRoyaltyNFT", {
-    args: ["Milady Maker", "Milady Maker"],
+    args: [
+      "Milady Maker",
+      "Milady Maker",
+      "https://www.miladymaker.net/milady/json/",
+    ],
     from: deployer,
     log: true,
   });
   await deploy("MockRoyaltyNFT", {
-    args: ["BoredApe", "BoredApe"],
+    args: [
+      "BoredApe",
+      "BoredApe",
+      "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/",
+    ],
     from: deployer,
     log: true,
   });
