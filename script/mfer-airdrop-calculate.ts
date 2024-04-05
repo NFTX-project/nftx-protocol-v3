04909b1855f2f874dfa459cf481a410ee1d9062b
apoorvlathey <latheyapoorv@gmail.com>
2024-04-05 19:43:38 +0530

    create mfer airdrop distribution scripts

---
+++ script/mfer-airdrop-calculate.ts
@@ -0,0 +1,303 @@
+// at block: 19541237
+// source: https://nftx.io/snapshot/
+const holdersSnapshot = [
+  {
+    Account: "0x029acd49d3aabc08402c84367c050dfd52b6c566",
+    Token: "xMFERWETH",
+    "MFER Balance": 1.6427596519722938,
+  },
+  {
+    Account: "0x434f5325ddcdbbfcce64be2617c72c4aa33ec3e7",
+    Token: "xMFERWETH",
+    "MFER Balance": 0.2175161685144107,
+  },
+  {
+    Account: "0x59de64f518f99b310e8e0014823b08e849eb7a24",
+    Token: "xMFERWETH",
+    "MFER Balance": 0.5739566626782264,
+  },
+  {
+    Account: "0xc6cf5a020bcfc65c6f8181c04cbb5ef5050fe28e",
+    Token: "xMFERWETH",
+    "MFER Balance": 4.464914549726532,
+  },
+  {
+    Account: "0x5ad6211cd3fde39a9cecb5df6f380b8263d1e277",
+    Token: "SLP",
+    "MFER Balance": 0.047638571895421554,
+  },
+  {
+    Account: "0x1662d66493ba8d210c2d129fbab0e8de04fe9ede",
+    Token: "xMFER",
+    "MFER Balance": 1.0346540393179777,
+  },
+  {
+    Account: "0x330e75e1f48b1ee968197cc870511665a4a5a832",
+    Token: "xMFER",
+    "MFER Balance": 33.69294296823258,
+  },
+  {
+    Account: "0x4eac46c2472b32dc7158110825a7443d35a90168",
+    Token: "xMFER",
+    "MFER Balance": 0.00123600242757302,
+  },
+  {
+    Account: "0x59de64f518f99b310e8e0014823b08e849eb7a24",
+    Token: "xMFER",
+    "MFER Balance": 0.27382239053466323,
+  },
+  {
+    Account: "0x8da02d597a2616e9ec0c82b2b8366b00d69da29a",
+    Token: "xMFER",
+    "MFER Balance": 0.06531312445238971,
+  },
+  {
+    Account: "0xb411b974c0ac75c88e5039ea0bf63a84aa7b5377",
+    Token: "xMFER",
+    "MFER Balance": 1.0807246075219052,
+  },
+  {
+    Account: "0xb4df44193a60586d9229bd6307a3241d2b04ce79",
+    Token: "xMFER",
+    "MFER Balance": 2.0670587390156117,
+  },
+  {
+    Account: "0xb72b37107db1ad520cab85204ef58a51981cb6ec",
+    Token: "xMFER",
+    "MFER Balance": 1.0857371015407047,
+  },
+  {
+    Account: "0xfce6abe45b291522cd0d7ceddf1dac2ee0283a6b",
+    Token: "xMFER",
+    "MFER Balance": 0.000145400018223575,
+  },
+  {
+    Account: "0x00000000003b3cc22af3ae1eac0440bcee416b40",
+    Token: "MFER",
+    "MFER Balance": 0.000001204082623226,
+  },
+  {
+    Account: "0x007933790a4f00000099e9001629d9fe7775b800",
+    Token: "MFER",
+    "MFER Balance": 0.00004,
+  },
+  {
+    Account: "0x0b54a3de68e0515cb85e44b4c688cb41e35335af",
+    Token: "MFER",
+    "MFER Balance": 0.000003151608654508,
+  },
+  {
+    Account: "0x0f6e4145e878ae9047d55c5f24c7337d27a9bc89",
+    Token: "MFER",
+    "MFER Balance": 0.002700364006729657,
+  },
+  {
+    Account: "0x17fb4db24b8f1817bc33d6e167b105fd77599c28",
+    Token: "MFER",
+    "MFER Balance": 0.006786276593339461,
+  },
+  {
+    Account: "0x1ba9c2d9053948290ae244ca97453dc29c55c1d6",
+    Token: "MFER",
+    "MFER Balance": 0.04934799842945904,
+  },
+  {
+    Account: "0x1e7d6b88c0aff66d8f1d6b649bd3b369e71ad527",
+    Token: "MFER",
+    "MFER Balance": 2.0242874083412006,
+  },
+  {
+    Account: "0x2741acb77d4e4d7fddd9144233f0a08f60171ad1",
+    Token: "MFER",
+    "MFER Balance": 0.00024785013906664,
+  },
+  {
+    Account: "0x3676eae386ce3a858e42e306383253b12ba81773",
+    Token: "MFER",
+    "MFER Balance": 0.051645629703778714,
+  },
+  {
+    Account: "0x391b2e210870f605be93932236900bc873baaf39",
+    Token: "MFER",
+    "MFER Balance": 0.000959289176640593,
+  },
+  {
+    Account: "0x3fc91a3afd70395cd496c647d5a6cc9d4b2b7fad",
+    Token: "MFER",
+    "MFER Balance": 0.000001173295421573,
+  },
+  {
+    Account: "0x4f764a08c66251e13bdd85b4bb0652b739736328",
+    Token: "MFER",
+    "MFER Balance": 0.011131487261288722,
+  },
+  {
+    Account: "0x5290f0d929de438799377fa956dea76cdef318b6",
+    Token: "MFER",
+    "MFER Balance": 0.000001589737880784,
+  },
+  {
+    Account: "0x52b1dc1dc9f952269b777436c4f0d27f0db13f61",
+    Token: "MFER",
+    "MFER Balance": 0.006822405176975611,
+  },
+  {
+    Account: "0x54d2562c8e9e970561a3cfd11e0b6e0977f135b8",
+    Token: "MFER",
+    "MFER Balance": 0.08193649651028286,
+  },
+  {
+    Account: "0x6a72e2a8c95031bc2cc2755e5a30a145645e024a",
+    Token: "MFER",
+    "MFER Balance": 0.000028076032178845,
+  },
+  {
+    Account: "0x6ad11f36d051ccf5bc06bc53356549bbfa61a1a4",
+    Token: "MFER",
+    "MFER Balance": 0.01264435147380309,
+  },
+  {
+    Account: "0x6d1305bb27232bc121c8075c92978edbba199c6a",
+    Token: "MFER",
+    "MFER Balance": 0.000001084377801451,
+  },
+  {
+    Account: "0x74be0af0bf7254328ddffc09425ff71d64a1a836",
+    Token: "MFER",
+    "MFER Balance": 1.77957959242e-7,
+  },
+  {
+    Account: "0x7dfe56f30d0c8fe31f2e698920311be6f80a825b",
+    Token: "MFER",
+    "MFER Balance": 0.00012968394497621,
+  },
+  {
+    Account: "0x8c8f1be5dbdfa432bbfb33d6a13779e889d8ccf9",
+    Token: "MFER",
+    "MFER Balance": 0.004384343925005452,
+  },
+  {
+    Account: "0x8da885d8a698843ca1f8a6cf38347990f0d84eb2",
+    Token: "MFER",
+    "MFER Balance": 0.024735881690214113,
+  },
+  {
+    Account: "0x9008d19f58aabd9ed0d60971565aa8510560ab41",
+    Token: "MFER",
+    "MFER Balance": 0.018535677805472845,
+  },
+  {
+    Account: "0x96f1daa32930933d503f93d270770f885de22d5c",
+    Token: "MFER",
+    "MFER Balance": 16.490973727972968,
+  },
+  {
+    Account: "0xaadad4324eefcf2e7e2c1b6bbbf0bad1f1775497",
+    Token: "MFER",
+    "MFER Balance": 0.00014893945215643,
+  },
+  {
+    Account: "0xb155b245c1a6fd99735b03ff1a8d8ecb165cce49",
+    Token: "MFER",
+    "MFER Balance": 4.4390018649e-8,
+  },
+  {
+    Account: "0xb34860f27167ed8110991e04e3e7177a58c74401",
+    Token: "MFER",
+    "MFER Balance": 0.1,
+  },
+  {
+    Account: "0xba386d72ee66b86c51ec92e75b6a32e2563bfd59",
+    Token: "MFER",
+    "MFER Balance": 0.000009169618879048,
+  },
+  {
+    Account: "0xbbc53022af15bb973ad906577c84784c47c14371",
+    Token: "MFER",
+    "MFER Balance": 0.000994301690499669,
+  },
+  {
+    Account: "0xc2e0ccfd52326d9db2ca7fe8ef11018953244e97",
+    Token: "MFER",
+    "MFER Balance": 0.000089271627660028,
+  },
+  {
+    Account: "0xc5dcdeea5248b9a136ee23d092142e0b59d0b047",
+    Token: "MFER",
+    "MFER Balance": 7.16775024293e-7,
+  },
+  {
+    Account: "0xcd4ebd768ccbe022274506f5e45d14fd90dc5be7",
+    Token: "MFER",
+    "MFER Balance": 0.013046201430498587,
+  },
+  {
+    Account: "0xd8042a9bb8ff398408d494f44a466574d0856b0f",
+    Token: "MFER",
+    "MFER Balance": 0.000002753702005009,
+  },
+  {
+    Account: "0xe044be24efa511730d9f70766d56d0fb1ce7b966",
+    Token: "MFER",
+    "MFER Balance": 0.025904632469967347,
+  },
+  {
+    Account: "0xe6932b2711df88e16a6e2a5223410d093c2a6bf2",
+    Token: "MFER",
+    "MFER Balance": 0.002621304647881677,
+  },
+  {
+    Account: "0xe87e115b86cb00b39361a12ac917b17e82ce4f0c",
+    Token: "MFER",
+    "MFER Balance": 0.6590124299982225,
+  },
+  {
+    Account: "0xf25041497082f71223e89d40efb75c66af87e408",
+    Token: "MFER",
+    "MFER Balance": 0.002626922486761772,
+  },
+  {
+    Account: "0xfce6abe45b291522cd0d7ceddf1dac2ee0283a6b",
+    Token: "MFER",
+    "MFER Balance": 9.999999999e-9,
+  },
+  {
+    Account: "0xfe88dd131d675874d8475ed6d3e3e92993f76d51",
+    Token: "MFER",
+    "MFER Balance": 0.005382741148154243,
+  },
+];
+
+const MFERToDistribute = 670_000;
+// account for floating point error
+const adjustment = 0.000000002;
+
+let totalVTokenSupply = 0;
+holdersSnapshot.map((holder) => {
+  totalVTokenSupply += holder["MFER Balance"];
+});
+
+const MFERPerVToken = MFERToDistribute / totalVTokenSupply;
+
+const addressTOMFER: {
+  [address: string]: number;
+} = {};
+holdersSnapshot.map((holder) => {
+  if (!addressTOMFER[holder.Account]) {
+    addressTOMFER[holder.Account] = 0;
+  }
+  addressTOMFER[holder.Account] += parseFloat(
+    (holder["MFER Balance"] * MFERPerVToken).toFixed(18)
+  );
+});
+
+let calculatedTotalToDistribute = 0;
+Object.keys(addressTOMFER).map((address) => {
+  calculatedTotalToDistribute += addressTOMFER[address];
+});
+const discrepancy = MFERToDistribute - calculatedTotalToDistribute;
+if (Math.abs(discrepancy) <= adjustment) {
+  addressTOMFER[Object.keys(addressTOMFER)[0]] += discrepancy;
+}
+
+console.log(addressTOMFER);
